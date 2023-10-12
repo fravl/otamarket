8974d0723568fff60c7cb3e458ebb365b67eb264
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-12 12:35:26 +0300

    add types for products

--- frontend/src/services/ProductService.ts
+++ frontend/src/services/ProductService.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
+import { Product } from "../types/Product";
 const baseUrl = "https://dummyjson.com/products";
 
-const getAll = async () => {
+const getAll = async (): Promise<Product[]> => {
     const request = axios.get(baseUrl);
     const response = await request;
     return response.data;
 };
 
-const getById = async (id: number) => {
+const getById = async (id: number): Promise<Product> => {
     const request = axios.get(`${baseUrl}/${id}`);
     const response = await request;
     return response.data;
