e3be5c30e604f02ba8fbb4a909788c17773dcfdb
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-12 11:59:34 +0300

    format all files with prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import Navbar from './Components/Navbar';
-import AllPage from './Components/AllPage';
-
+import React from "react";
+import Navbar from "./Components/Navbar";
+import AllPage from "./Components/AllPage";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <AllPage/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <AllPage />
+        </div>
+    );
 }
 
 export default App;
