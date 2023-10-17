0a5f389430cbdda912ad97cb42ab06dece41c727
Jaakko Soitinaho <jaakko.soitinaho@veikkaus.fi>
2023-10-17 16:22:54 +0300

    Revert "Restore Navbar"
    This reverts commit 39fa1d1f78cad802c8687927e35da62e8e01df08.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import {useState, useEffect} from 'react'
-import Navbar from "./components/Navbar";
+import React, {useState, useEffect} from 'react'
 import ProductService from './services/ProductService'
 import { Product } from './types/Product'
 import AppHeader from './components/AppHeader';
@@ -21,7 +20,6 @@ function App() {
     return (
         <>
             <AppHeader />
-            <Navbar />
             <Outlet context={{products} satisfies ProductOutletContext}/>
         </>
     );
