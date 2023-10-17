431ee4e57476ca7bf5a6153fb63e3535cdef37b9
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-17 11:56:21 +0300

    clean-up AddItemFab

--- frontend/src/components/AddItemFab.tsx
+++ frontend/src/components/AddItemFab.tsx
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom";
 const AddItemFab = () => (
     <Link
         to="/add"
-        className="btn btn-primary rounded-circle floating-btn"
-        data-bs-toggle="tooltip"
-        data-bs-placement="left"
+        className="btn btn-primary rounded-circle"
         title="Add New Item"
         style={{ position: "fixed", bottom: "20px", right: "20px" }}
     >
