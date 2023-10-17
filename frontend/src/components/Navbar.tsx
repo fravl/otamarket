0917060c022204c8d5d16e91f11c9690b5a15a5c
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-17 10:19:34 +0300

    implement static navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,35 @@
-import React from "react";
-
 const Navbar = () => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-                OtaMarket
-            </a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div className="container ">
+            <ul className="nav container d-flex justify-content-around align-items-center">
+                <li className="nav-item">
+                    <a
+                        href="#"
+                        className="nav-link link-dark px-2 active"
+                        aria-current="page"
+                    >
+                        All
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#"
+                        className="nav-link link-dark px-2 active"
+                        aria-current="page"
+                    >
+                        Sales
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#"
+                        className="nav-link link-dark px-2 active"
+                        aria-current="page"
+                    >
+                        Claims
+                    </a>
+                </li>
+            </ul>
         </div>
     </nav>
 );
