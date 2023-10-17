b269225683640750ffd68c614269cb76440f4d1c
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-17 11:42:00 +0300

    basic router setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
+import ErrorPage from "./components/ErrorPage";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement,
 );
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+    },
+]);
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 );
