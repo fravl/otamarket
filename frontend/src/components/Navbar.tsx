6b77d53c45e3eef368f422a938f8d5e93c2c65b3
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-17 12:18:58 +0300

    remove unneeded div in Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,29 +2,23 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => (
     <nav className="navbar navbar-light bg-light border-bottom">
-        <div className="container ">
-            <ul className="navbar-nav container d-flex flex-row justify-content-around align-items-center">
-                <li className="nav-item">
-                    <Link
-                        to="/"
-                        className="nav-link active"
-                        aria-current="page"
-                    >
-                        All
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/" className="nav-link">
-                        Sales
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/" className="nav-link">
-                        Claims
-                    </Link>
-                </li>
-            </ul>
-        </div>
+        <ul className="navbar-nav container d-flex flex-row justify-content-around align-items-center">
+            <li className="nav-item">
+                <Link to="/" className="nav-link active" aria-current="page">
+                    All
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/" className="nav-link">
+                    Sales
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/" className="nav-link">
+                    Claims
+                </Link>
+            </li>
+        </ul>
     </nav>
 );
 
