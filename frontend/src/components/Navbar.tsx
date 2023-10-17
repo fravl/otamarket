5e754d08689855036ca7189270c43f5089580777
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-17 10:30:35 +0300

    highlight current page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,31 +1,19 @@
 const Navbar = () => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+    <nav className="navbar navbar-light bg-light border-bottom">
         <div className="container ">
-            <ul className="nav container d-flex justify-content-around align-items-center">
+            <ul className="navbar-nav container d-flex flex-row justify-content-around align-items-center">
                 <li className="nav-item">
-                    <a
-                        href="#"
-                        className="nav-link link-dark px-2 active"
-                        aria-current="page"
-                    >
+                    <a href="#" className="nav-link active" aria-current="page">
                         All
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a
-                        href="#"
-                        className="nav-link link-dark px-2 active"
-                        aria-current="page"
-                    >
+                    <a href="#" className="nav-link">
                         Sales
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a
-                        href="#"
-                        className="nav-link link-dark px-2 active"
-                        aria-current="page"
-                    >
+                    <a href="#" className="nav-link">
                         Claims
                     </a>
                 </li>
