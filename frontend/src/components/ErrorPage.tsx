e7bd785c7467830aa7e8831d31283fd8fa71eed6
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-17 11:47:07 +0300

    style error page

--- frontend/src/components/ErrorPage.tsx
+++ frontend/src/components/ErrorPage.tsx
@@ -19,10 +19,10 @@ export default function ErrorPage() {
     }
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
+        <div id="error-page" className="container">
+            <h1 className="display-4">Oops!</h1>
+            <p className="lead">Sorry, an unexpected error has occurred.</p>
+            <p className="lead">
                 <i>{errorMessage}</i>
             </p>
         </div>
