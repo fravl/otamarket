9fdabacc44691ba0ed21242850a070c977352e3a
DijamGoran <diamgoran@gmail.com>
2023-10-24 17:46:39 +0300

    removed unnecessary class name

--- frontend/src/components/AddItemFab.tsx
+++ frontend/src/components/AddItemFab.tsx
@@ -7,7 +7,7 @@ const AddItemFab = () => (
         title="Add New Item"
         style={{ position: "fixed", bottom: "20px", right: "20px", width: "5rem", height: "5rem", textAlign:"center" }}
     >
-        <i className="bi bi-plus-lg addbutton" style={{ position: "relative", bottom:"10%", fontSize: "3rem" }}></i>
+        <i className="bi bi-plus-lg" style={{ position: "relative", bottom:"10%", fontSize: "3rem" }}></i>
     </Link>
 );
 
