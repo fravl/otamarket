507d022a4511291564fc2f9c2f0657f86c1bfdaf
DijamGoran <diamgoran@gmail.com>
2023-10-24 18:08:57 +0300

    Formatting code and placeholder tabs to test navlinks

--- frontend/src/components/AddItemFab.tsx
+++ frontend/src/components/AddItemFab.tsx
@@ -5,9 +5,19 @@ const AddItemFab = () => (
         to="/add"
         className="btn btn-primary rounded-circle"
         title="Add New Item"
-        style={{ position: "fixed", bottom: "20px", right: "20px", width: "5rem", height: "5rem", textAlign:"center" }}
+        style={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+            width: "5rem",
+            height: "5rem",
+            textAlign: "center",
+        }}
     >
-        <i className="bi bi-plus-lg" style={{ position: "relative", bottom:"10%", fontSize: "3rem" }}></i>
+        <i
+            className="bi bi-plus-lg"
+            style={{ position: "relative", bottom: "10%", fontSize: "3rem" }}
+        ></i>
     </Link>
 );
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,11 @@
 import { NavLink } from "react-router-dom";
 
-
 const Navbar = () => {
-
-
-    return(
+    return (
         <nav className="navbar navbar-light bg-light border-bottom">
             <ul className="navbar-nav container d-flex flex-row justify-content-around align-items-center">
                 <li className="nav-item">
-                    <NavLink to="/"  className="nav-link" aria-current="page">
+                    <NavLink to="/" className="nav-link" aria-current="page">
                         All
                     </NavLink>
                 </li>
@@ -18,13 +15,13 @@ const Navbar = () => {
                     </NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to="/placeholder" className="nav-link">
+                    <NavLink to="/placeholder2" className="nav-link">
                         Claims
                     </NavLink>
                 </li>
             </ul>
         </nav>
-    )
-    };
+    );
+};
 
 export default Navbar;
