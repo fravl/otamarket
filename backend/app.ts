0b37a81a55b803c4d251d92dd7b6301ad6566cb3
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-10-25 15:23:42 +0300

    move dummyjson call to backend

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import https from "https";
 import "dotenv/config";
 
 const app: Express = express();
@@ -7,7 +8,20 @@ const app: Express = express();
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello from backend");
+    https
+        .get("https://dummyjson.com/products", (response) => {
+            let data = "";
+            response.on("data", (chunk) => {
+                data += chunk;
+            });
+            response.on("end", () => {
+                res.send(data);
+            });
+        })
+        .on("error", (err) => {
+            console.log("Error: " + err.message);
+            res.status(500).send("Error fetching data from external API");
+        });
 });
 
 const PORT = process.env.NODE_DOCKER_PORT ?? 8080;
