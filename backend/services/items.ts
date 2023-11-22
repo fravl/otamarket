866fe28f743f12188e338870812bb32739506490
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-11-22 11:05:09 +0200

    formatting

--- backend/services/items.ts
+++ backend/services/items.ts
@@ -4,34 +4,29 @@ import { Item } from "../db/models";
 
 export async function all(): Promise<any> {
     const items = await db.items.all();
-    
-    const dtos = items.map(
-        (item) =>  {
-            if(item.thumbnail_id === null){
-               return  new ItemSummary(item, item.claim_count)
-            } else {
-               return  new ItemSummary(item, item.claim_count, item.thumbnail)
-            }
+
+    const dtos = items.map((item) => {
+        if (item.thumbnail_id === null) {
+            return new ItemSummary(item, item.claim_count);
+        } else {
+            return new ItemSummary(item, item.claim_count, item.thumbnail);
         }
-    );
+    });
 
     return dtos;
 }
 
 export async function allCategories(): Promise<any> {
     const categories = await db.items.allCategories();
-    
+
     return categories;
 }
 
 export async function allItemCategories(): Promise<any> {
     const categories = await db.items.allItemCategories();
-    
+
     return categories;
 }
-
-
-
 
 export async function findById(id: number): Promise<ItemDetails | null> {
     const item = await db.items.findById(id);
@@ -46,12 +41,15 @@ export async function findById(id: number): Promise<ItemDetails | null> {
     }
 }
 
-export async function addItem(item: Omit<Item, "id">, categories: string[]): Promise<any> {
+export async function addItem(
+    item: Omit<Item, "id">,
+    categories: string[],
+): Promise<any> {
     const res = await db.items.addItem(item);
 
     const addedItemId: number = res.id;
 
-    for(const category of categories){
-        await db.items.addToCategory(addedItemId, parseInt(category))
+    for (const category of categories) {
+        await db.items.addToCategory(addedItemId, parseInt(category));
     }
 }
